1252a9be37f3fb7e14729a5a48509541d9db6d78
tiagojaconi <tiagojaconi@ucl.br>
2020-05-07 23:21:13 +0000

    Classe Jogador, com 2 construtores apenas.

--- main.cs
+++ main.cs
@@ -1,5 +1,32 @@
 using System;
 
+class Jogador{
+	private string nomeJogador;
+	private string senhaJogador;
+	protected int idadeJogador;
+	protected float pesoJogador;
+	protected float alturaJogador;
+	private int contJogador = 0;
+
+	Jogador(){
+		contJogador++;
+		nomeJogador = ("Jogador "+contJogador);
+		senhaJogador = " ";
+		idadeJogador = 15;
+		alturaJogador = 1.4f;
+		pesoJogador = 40;
+	} 
+	Jogador(string nome, string senha, int idade, float altura, float peso){
+		contJogador++;
+		nomeJogador = nome;
+		senhaJogador = senha;
+		idadeJogador = idade;
+		alturaJogador = altura;
+		pesoJogador = peso;
+	}
+	
+}
+
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
